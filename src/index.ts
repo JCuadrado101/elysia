7c2c7d777569ab1552069bacd0187d9b248c9bc0
jordancuadrado <jac59155@gmail.com>
2023-10-09 21:37:50 -0400

    few examples

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,48 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import { swagger } from "@elysiajs/swagger"
+
+const getUserById = (id: string) => {
+  return `This is ${id}`
+}
+
+const signIn = (credentials: any) => {
+  return credentials.username === 'admin' && credentials.password === 'password';
+}
 
 new Elysia()
+  .use(swagger(
+    {
+      path: "/swagger"
+    }
+  ))
   .get("/", () => "hello world")
   .get("test", () => "new page")
+
+  // route and params with context
+  .get('/id/:id', (context) => context.params.id) 
+
+  .post(
+    "/profile",
+    ({ body }) => body,
+    {
+      body: t.Object({
+        username: t.String()
+      })
+    }
+  )
+
+  // hander example
+
+  .post('/sign', ({ body, set }) => {
+    const signed = signIn(body);
+
+    if(signed)
+      return "Welcome back"
+
+      set.status = 403
+      return "Invalid username and password"
+  })
+
   .listen(3000)
 
   console.log("port 3000");
